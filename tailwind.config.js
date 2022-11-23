4f2016b078b20990b223e847a6cc33292d372089
codernoyan <coder.noyan@gmail.com>
2022-11-24 01:07:16 +0600

    added banner

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      // colors: {
+      //   primary: '#480CA8',
+      //   secondary: '#560BAD',
+      // }
+    },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
